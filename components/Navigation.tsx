f458a01ead261fb3e5e0c6a4abda31ba110964a9
Kriss Gardner <krissrobert12@gmail.com>
2024-06-25 19:17:32 +0300

    Add styling

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link'
 
 const Navigation = () => {
   return (
-    <div className="text-white flex gap-5">
+    <div className="text-white flex gap-5 font-light mb-16">
       <Link href="/">home</Link>
       <Link href="/work">work</Link>
       <Link href="/contact">contact</Link>
