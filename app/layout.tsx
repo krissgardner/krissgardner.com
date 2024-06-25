543c8d435e3d7dae0289c2caa897abf6f47b0fe5
Kriss Gardner <krissrobert12@gmail.com>
2024-06-25 18:17:53 +0300

    Add prettier

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,23 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: "Kriss Gardner",
-  description: "Hey! I'm Kriss Gardner. I'm a full stack developer & community builder.",
-};
+  title: 'Kriss Gardner',
+  description:
+    "Hey! I'm Kriss Gardner. I'm a full stack developer & community builder.",
+}
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
     </html>
-  );
+  )
 }
