2cdc8cf07195798ee1d934f3b237362967c665fe
Kriss Gardner <krissrobert12@gmail.com>
2024-06-25 19:13:05 +0300

    Create basic nav

---
+++ components/Navigation.tsx
@@ -0,0 +1,14 @@
+import Link from 'next/link'
+
+const Navigation = () => {
+  return (
+    <div className="text-white flex gap-5">
+      <Link href="/">home</Link>
+      <Link href="/work">work</Link>
+      <Link href="/contact">contact</Link>
+      <Link href="/newsletter">newsletter</Link>
+    </div>
+  )
+}
+
+export default Navigation
