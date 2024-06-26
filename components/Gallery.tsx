9a2f7edffe7e2417b152b7761bad1874bdbb9fbe
Kriss Gardner <krissrobert12@gmail.com>
2024-06-26 15:41:54 +0300

    Change alt

--- components/Gallery.tsx
+++ components/Gallery.tsx
@@ -12,7 +12,7 @@ const Gallery = () => {
     <div className="grid grid-cols-2 grid-rows-4 sm:grid-rows-3 sm:grid-cols-3 gap-4 my-8">
       <div className="relative h-40">
         <Image
-          alt="Image 1"
+          alt="Me working on incredible freelance projects! If you want to work with me, send your request over email."
           src={working}
           fill
           sizes="(max-width: 768px) 213px, 33vw"
@@ -22,7 +22,7 @@ const Gallery = () => {
       </div>
       <div className="relative sm:row-span-2 row-span-1">
         <Image
-          alt=""
+          alt="Me at a networking event with other freelancers."
           src={evening}
           fill
           sizes="(max-width: 768px) 213px, 33vw"
@@ -32,7 +32,7 @@ const Gallery = () => {
       </div>
       <div className="relative">
         <Image
-          alt=""
+          alt="Me talking to other freelancers at a meetup. My friend and co-founder, Andrei Stoica, is next to me."
           src={meetup}
           fill
           sizes="(max-width: 768px) 213px, 33vw"
@@ -42,7 +42,7 @@ const Gallery = () => {
       </div>
       <div className="relative row-span-2">
         <Image
-          alt=""
+          alt="Me recording content for my online mentorship skool Tenxbase."
           src={creator}
           fill
           sizes="(max-width: 768px) 213px, 33vw"
@@ -52,7 +52,7 @@ const Gallery = () => {
       </div>
       <div className="relative row-span-2">
         <Image
-          alt=""
+          alt="Me holding a weekly Q&A with some of my students."
           src={lounge}
           fill
           sizes="(max-width: 768px) 213px, 33vw"
@@ -62,7 +62,7 @@ const Gallery = () => {
       </div>
       <div className="relative h-40">
         <Image
-          alt=""
+          alt="Me having a blast at one of my favourite lounges!"
           src={mentorship}
           fill
           sizes="(max-width: 768px) 213px, 33vw"
