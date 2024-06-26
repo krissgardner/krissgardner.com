ffa255c94adf4960377f1a472849ba409027929d
Kriss Gardner <krissrobert12@gmail.com>
2024-06-26 13:30:54 +0300

    Create Home Page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,14 @@
+import Gallery from '@/components/Gallery'
+
 export default function Page() {
-  return <main></main>
+  return (
+    <main>
+      <h1 className="mb-8 text-2xl font-medium tracking-tighter text-white">
+        {`hey, I'm kriss 👋`}
+      </h1>
+      <p>{`I'm a full stack developer, content creator and startup enthusiast. I help businesses achieve their goals through product-driven development, effective cost management and strategic decision-making.`}</p>
+
+      <Gallery />
+    </main>
+  )
 }
