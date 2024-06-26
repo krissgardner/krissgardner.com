b93e1c990b665c350a16248f279c4060289daae9
Kriss Gardner <krissrobert12@gmail.com>
2024-06-26 13:31:29 +0300

    Create 404 page

---
+++ app/not-found.tsx
@@ -0,0 +1,13 @@
+export default function NotFound() {
+  return (
+    <main>
+      <h1 className="font-medium text-2xl mb-8 tracking-tighter">
+        {`Oh no! This page doesn't exist.`}
+      </h1>
+      <p>
+        If you expected to see something here, let me know
+        (contact@krissgardner.com).
+      </p>
+    </main>
+  )
+}
