4138b78ed2d3a290f0563df9d840b748becd2b32
Kriss Gardner <krissrobert12@gmail.com>
2024-06-28 02:33:56 +0300

    Add speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { GeistSans } from 'geist/font/sans'
 import { GeistMono } from 'geist/font/mono'
 import Navigation from '@/components/Navigation'
 import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/next'
 
 import './globals.css'
 
@@ -27,6 +28,7 @@ export default function RootLayout({
         </div>
 
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   )
