beceabcdb6fae80ac355f1c1033a7870dfc4e6a2
Kriss Gardner <krissrobert12@gmail.com>
2024-06-27 14:36:50 +0300

    add more text

--- app/work/page.tsx
+++ app/work/page.tsx
@@ -16,14 +16,16 @@ const Work = () => {
       <hr />
 
       <h2 className="font-medium text-xl mb-1 tracking-tighter">Therapedia</h2>
-      <p className="text-neutral-400 text-sm mb-4">Technical Lead</p>
+      <p className="text-neutral-400 text-sm mb-4">
+        Freelance Full Stack Developer, May 2024 - Jun 2024
+      </p>
 
       <p className="my-5">
         Joined the Therapedia team to help them create the best AI assistant
         tailored for therapists, psychologists, and psychiatrists.
       </p>
 
-      <ul>
+      <ul className="list-dark">
         <li>
           I developed a realtime transcription application with built in
           sentence analysis which resulted in precise transcriptions. Created
@@ -46,7 +48,7 @@ const Work = () => {
         Crimson Education
       </h2>
       <p className="text-neutral-400 text-sm mb-4">
-        Freelance Full Stack Developer
+        Freelance Full Stack Developer, Aug 2023 - Apr 2024
       </p>
 
       <p className="my-5">
@@ -55,7 +57,7 @@ const Work = () => {
         facilitate the acquisition of Admityogi and take lead in development.
       </p>
 
-      <ul>
+      <ul className="list-dark">
         <li>
           Increased the total traffic by 175% by completing the merger of
           Admityogi and Dr.Ivy platforms in only 2 weeks.
@@ -75,6 +77,39 @@ const Work = () => {
         <li>
           Developed industry leading technology by implementing OpenAI’s API for
           Essay Revision Tool & ChanceMe.
+        </li>
+      </ul>
+
+      <hr />
+
+      <h2 className="font-medium text-xl mb-1 tracking-tighter">Genova.ai</h2>
+      <p className="text-neutral-400 text-sm mb-4">
+        Freelance Full Stack Developer, Jun 2023 - Oct 2023
+      </p>
+
+      <p className="my-5">
+        Venture studio building products around Generative AI
+      </p>
+
+      <ul className="list-dark">
+        <li>
+          Improved output quality by 10% by developing AI picture filtering &
+          detection before upload.
+        </li>
+        <li>
+          Reduced client tickets by over 60% by creating feedback & rewards
+          systems.
+        </li>
+        <li>
+          Increased traffic and incentivised users to share the app by
+          implementing a referral code feature.
+        </li>
+        <li>
+          Elevated user data collection with a system of prompt selection
+          interfaces.
+        </li>
+        <li>
+          Developed time sensitive features for both iOS and Android devices.
         </li>
       </ul>
     </div>
